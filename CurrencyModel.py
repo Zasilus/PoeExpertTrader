6b5211d60be1fdb54d9fccfdd7d47c83ce4d18b6
Francis O'Brien <fgo3@case.edu>
2020-10-21 16:28:48 -0400

    Added JSON parser, adds card name and chaos equivalent to a dictionary called currentStats

--- CurrencyModel.py
+++ CurrencyModel.py
@@ -4,4 +4,13 @@ import json
 resp = requests.get("https://poe.ninja/api/data/currencyoverview?league=Heist&type=Currency")
 raw_data = resp.content
 json_result = json.loads(raw_data)
-print(json_result["lines"][0])
+for n in range(len(json_result)):
+    print(json_result["lines"][n])
+
+currentStats = dict()
+for x in range(len(json_result)):
+    this_card = json_result["lines"][x]['currencyTypeName']
+    this_price = json_result["lines"][x]['chaosEquivalent']
+    currentStats.update({this_card:this_price})
+
+print(currentStats)
