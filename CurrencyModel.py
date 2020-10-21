77880d039203ffe49764d6faf869aec8f9d031fe
Francis O'Brien <fgo3@case.edu>
2020-10-21 16:40:10 -0400

    Fixed parsing range error, dictionary now holds all cards

--- CurrencyModel.py
+++ CurrencyModel.py
@@ -4,11 +4,12 @@ import json
 resp = requests.get("https://poe.ninja/api/data/currencyoverview?league=Heist&type=Currency")
 raw_data = resp.content
 json_result = json.loads(raw_data)
-for n in range(len(json_result)):
-    print(json_result["lines"][n])
+print(range(len(json_result["lines"])))
+#for n in range(len(json_result["lines"])):
+    #print(json_result["lines"][n])
 
 currentStats = dict()
-for x in range(len(json_result)):
+for x in range(len(json_result["lines"])):
     this_card = json_result["lines"][x]['currencyTypeName']
     this_price = json_result["lines"][x]['chaosEquivalent']
     currentStats.update({this_card:this_price})
