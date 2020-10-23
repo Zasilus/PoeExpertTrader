d09a81c3d04a8235a1685ed4025e03716b433fd0
Zasilus <asg101@case.edu>
2020-10-22 20:23:41 -0400

    Removed some unnecessary comments and new lines
    Signed-off-by: Zasilus <asg101@case.edu>

--- CurrencyModel.py
+++ CurrencyModel.py
@@ -3,14 +3,6 @@ import json
 
 
 currencyStats = dict()
-# resp = requests.get("https://poe.ninja/api/data/currencyoverview?league=Heist&type=Currency")
-# raw_data = resp.content
-# json_result = json.loads(raw_data)
-#for n in range(len(json_result["lines"])):
-    #print(json_result["lines"][n])
-
-
-print(currencyStats)
 
 def main():
     pullCurrency()
@@ -22,7 +14,6 @@ def pullCurrency():
     global currencyStats
     #resp = requests.get("https://poe.ninja/api/data/currencyoverview?league=Heist&type=Currency")
     #raw_data = resp.content
-    #raw_data = '{"lines":[{"currencyTypeName":"Mirror of Kalandra","pay":{"id":0,"league_id":86,"pay_currency_id":22,"get_currency_id":1,"sample_time_utc":"2020-10-22T18:43:01.6556515Z","count":64,"value":0.0000653268000000000000000000,"data_point_count":1,"includes_secondary":true},"receive":{"id":0,"league_id":86,"pay_currency_id":1,"get_currency_id":22,"sample_time_utc":"2020-10-22T18:43:01.6556515Z","count":75,"value":14634.622641509433962264150943,"data_point_count":1,"includes_secondary":true},"paySparkLine":{"data":[0.0,0.0,16.67,16.67,16.67,0.0,7.15],"totalChange":7.15},"receiveSparkLine":{"data":[0.0,0.39,4.86,8.96,8.13,-2.73,-3.66],"totalChange":-3.66},"chaosEquivalent":14944.51,"lowConfidencePaySparkLine":{"data":[0.0,0.0,16.67,16.67,16.67,0.0,7.15],"totalChange":7.15},"lowConfidenceReceiveSparkLine":{"data":[0.0,0.39,4.86,8.96,8.13,-2.73,-3.66],"totalChange":-3.66},"detailsId":"mirror-of-kalandra"}]}'    
     f = open("currencyoverview.json", "r")
     raw_data = f.read()
     json_result = json.loads(raw_data)
