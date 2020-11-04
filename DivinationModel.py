6f3206ac12064a6dfe668022105a92b235cd41d6
Francis O'Brien <fgo3@case.edu>
2020-11-04 16:46:55 -0500

    Added calculatePricePerStack method and started profit per stack method

--- DivinationModel.py
+++ DivinationModel.py
@@ -17,3 +17,21 @@ def pullDivination():
         divinationStats.update({this_card:[this_value,this_trade,this_stack]})
 
     print(divinationStats)
+
+def calculatePricePerStack():
+    global divinationStats
+    for x in divinationStats:
+        basePrice = divinationStats[x][1]
+        stackSize = divinationStats[x][3]
+        this_stackPrice = basePrice * stackSize
+        divinationStats[x].update({"StackPrice":this_stackPrice})
+
+""" def calculateProfitPerStack():
+    global divinationStats
+    from CurrencyModel import currencyStats
+    for x in divinationStats:
+        this_modifier = divinationStats[x][2]
+        this_stackPrice = divinationStats[x]["StackPrice"]
+        this_modPrice = 0
+        if this_modifier in currencyStats:
+            this_modPrice =  """
