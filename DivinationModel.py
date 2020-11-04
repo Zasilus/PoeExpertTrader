21e7236b682d69b56f00fd94a576561621e3dfa8
Francis O'Brien <fgo3@case.edu>
2020-11-04 15:59:51 -0500

    Minor changes to divination model, put code into function

--- DivinationModel.py
+++ DivinationModel.py
@@ -1,16 +1,19 @@
 import requests
 import json
 
-respDiv = requests.get("https://poe.ninja/api/data/itemoverview?league=Blight&type=DivinationCard")
-raw_div_data = respDiv.content
-json_div_results = json.loads(raw_div_data)
-
 divinationStats = dict()
-for x in range(len(json_div_results["lines"])):
-    this_card = json_div_results["lines"][x]['name']
-    this_value = json_div_results["lines"][x]['chaosValue']
-    this_trade = json_div_results["lines"][x]['explicitModifiers']
-    this_stack = json_div_results["lines"][x]['stackSize']
-    divinationStats.update({this_card:[this_value,this_trade,this_stack]})
 
-print(divinationStats)
+def pullDivination():
+    global divinationStats
+    respDiv = requests.get("https://poe.ninja/api/data/itemoverview?league=Blight&type=DivinationCard")
+    raw_div_data = respDiv.content
+    json_div_results = json.loads(raw_div_data)
+
+    for x in range(len(json_div_results["lines"])):
+        this_card = json_div_results["lines"][x]['name']
+        this_value = json_div_results["lines"][x]['chaosValue']
+        this_trade = json_div_results["lines"][x]['explicitModifiers']
+        this_stack = json_div_results["lines"][x]['stackSize']
+        divinationStats.update({this_card:[this_value,this_trade,this_stack]})
+
+    print(divinationStats)
