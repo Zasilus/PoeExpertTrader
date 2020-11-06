63dcbf4c9055b715d17cf0a935fabcddd06cc920
Zasilus <asg101@case.edu>
2020-11-05 19:30:01 -0500

    Put the models into classes to follow an object oriented design
    Signed-off-by: Zasilus <asg101@case.edu>

--- DivinationModel.py
+++ DivinationModel.py
@@ -1,76 +1,83 @@
 import requests
 import json
+from CurrencyModel import CurrencyModel
 
-divinationStats = dict()
-uniqueItems = dict()
+class DivinationModel:
+    divinationStats = dict()
+    uniqueItems = dict()
+    currencyModel = CurrencyModel()
+    def __init__(self):
+        self.pullDivination()
+        print("Divination Cards Pulled")
+        self.pullUniqueStats()
+        self.getItemValue("Exalted Orb")
 
-def main():
-    pullDivination()
-    print("Divination Cards Pulled")
-    pullUniqueStats()
+    def pullDivination(self):
+        f = open("divinationoverview.json", "r")
+        raw_data = f.read()
+        #respDiv = requests.get("https://poe.ninja/api/data/itemoverview?league=Blight&type=DivinationCard")
+        #raw_div_data = respDiv.content
+        f.close()
+        json_div_results = json.loads(raw_data)
 
-def pullDivination():
-    global divinationStats
-    f = open("divinationoverview.json", "r")
-    raw_data = f.read()
-    #respDiv = requests.get("https://poe.ninja/api/data/itemoverview?league=Blight&type=DivinationCard")
-    #raw_div_data = respDiv.content
-    f.close()
-    json_div_results = json.loads(raw_data)
+        for x in range(len(json_div_results["lines"])):
+            if(json_div_results["lines"][x]['chaosValue'] > 1):
+                this_card = json_div_results["lines"][x]['name']
+                this_value = json_div_results["lines"][x]['chaosValue']
+                this_trade = json_div_results["lines"][x]['explicitModifiers'][0]["text"]
+                this_trade = this_trade[this_trade.find("{")+1:this_trade.find("}")]
+                this_stack = json_div_results["lines"][x]['stackSize']
+                div_entry = {"cost":this_value, "item":this_trade, "stackSize":this_stack}
+                self.divinationStats.update({this_card:div_entry})
+       # print(self.divinationStats)
 
-    for x in range(len(json_div_results["lines"])):
-        if(json_div_results["lines"][x]['chaosValue'] > 1):
-            this_card = json_div_results["lines"][x]['name']
-            this_value = json_div_results["lines"][x]['chaosValue']
-            this_trade = json_div_results["lines"][x]['explicitModifiers'][0]["text"]
-            this_trade = this_trade[this_trade.find("{")+1:this_trade.find("}")]
-            this_stack = json_div_results["lines"][x]['stackSize']
-            div_entry = {"cost":this_value, "item":this_trade, "stackSize":this_stack}
-            divinationStats.update({this_card:div_entry})
-    
-    print(divinationStats)
+    def pullUniqueStats(self):
+        self.updateUniqueDict("uniquearmor.json")
+        self.updateUniqueDict("uniqueweapon.json")
+        self.updateUniqueDict("uniqueaccessory.json")
+        self.updateUniqueDict("uniqueflask.json")
+        self.updateUniqueDict("uniquejewel.json")
+        self.updateUniqueDict("prophecyoverview.json")
 
-def pullUniqueStats():
-    global uniqueItems
-    updateUniqueDict("uniquearmor.json")
-    updateUniqueDict("uniqueweapon.json")
-    updateUniqueDict("uniqueaccessory.json")
-    updateUniqueDict("uniqueflask.json")
-    updateUniqueDict("uniquejewel.json")
-    updateUniqueDict("prophecyoverview.json")
+    def updateUniqueDict(self, fileName):
+        f = open(fileName, "r")
+        raw_data = f.read()
+        f.close()
+        json_result = json.loads(raw_data)
 
-def updateUniqueDict(fileName):
-    global uniqueItems
-    f = open(fileName, "r")
-    raw_data = f.read()
-    f.close()
-    json_result = json.loads(raw_data)
+        for x in range(len(json_result["lines"])):
+            entry = json_result["lines"][x]
+            #We are limiting the entries to non 6 links and chaosValues over 1 as theres no point
+            #in attempting to flip below the 2 chaos threshold and 6 links are a different much
+            #rarer form of the item that 
+            if entry["links"] != 6 and entry['chaosValue'] > 2:
+                this_name = entry['name']
+                this_value = entry['chaosValue']
+                self.uniqueItems.update({this_name:this_value})
 
-    for x in range(len(json_result["lines"])):
-        entry = json_result["lines"][x]
-        #We are limiting the entries to non 6 links and chaosValues over 1 as theres no point
-        #in attempting to flip below the 2 chaos threshold and 6 links are a different much
-        #rarer form of the item that 
-        if entry["links"] != 6 and entry['chaosValue'] > 2:
-            this_name = entry['name']
-            this_value = entry['chaosValue']
-            uniqueItems.update({this_name:this_value})
+    def calculatePricePerStack(self):
+        for x in self.divinationStats:
+            basePrice = self.divinationStats[x]["cost"]
+            stackSize = self.divinationStats[x]["stackSize"]
+            totalCost = basePrice * stackSize
+            self.divinationStats[x].update({"StackPrice":totalCost})
 
-def calculatePricePerStack():
-    global divinationStats
-    for x in divinationStats:
-        basePrice = divinationStats[x]["cost"]
-        stackSize = divinationStats[x]["stackSize"]
-        totalCost = basePrice * stackSize
-        divinationStats[x].update({"StackPrice":totalCost})
+    def calculateProfitPerStack(self):
+        return ""
 
-def calculateProfitPerStack():
-    return ""
+    def getItemValue(self,itemName):
+        #Check the Unique dictionary to see if its an item that exists inside it
+        if itemName in self.uniqueItems:
+            print(self.uniqueItems[itemName])
+            return self.uniqueItems[itemName]
+        else:
+            currencyData = self.currencyModel.getCurrencyData()
+            print(currencyData)
+            if itemName in currencyData:
+                print(currencyData[itemName]["ChaosEquivalent"])
+                return currencyData[itemName]["ChaosEquivalent"]
+            
 
-def getItemValue(itemName):
-    global uniqueItems
-    if uniqueItems[itemName] not None:
-        return uniqueItems[itemName]
 
 """ def calculateProfitPerStack():
     global divinationStats
@@ -83,4 +90,4 @@ def getItemValue(itemName):
             this_modPrice =  """
 
 if __name__ == '__main__':
-    main()
+    divModel = DivinationModel()
