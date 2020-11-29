7474adf517d60e724a889942fe6cbe74836d985a
Francis O'Brien <fgo3@case.edu>
2020-11-29 17:56:19 -0500

    Fixed calculateCurrencyOptimal

--- Controller.py
+++ Controller.py
@@ -45,10 +45,10 @@ class Controller:
         counter = 0
         for x in self.controllerCStats:
             currency_Name = currency_List[counter]
-            currency_Get = math.floor(chaosAmount/self.controllerCStats[x]['pay'])
+            currency_Get = math.floor(chaosAmount/self.controllerCStats[x]['receive'])
             if currency_Get > 1000:
                 currency_Get = 1000
-            currency_Return = currency_Get * self.controllerCStats[x]['ROI']
+            currency_Return = currency_Get * self.controllerCStats[x]['difference']
             #print("Current optimal is ", optimal_Currency, " at ", optimal_value)
             #print(currency_Name, " is worth ", currency_Return)
             if (currency_Return > optimal_value):
